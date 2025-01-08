574c80b138066c934965cf1a16405d3e18bc9bd8
EyachirArafat <me.eyachirarafat@gmail.com>
2025-01-08 20:45:05 +0600

    BIGallery modal

--- src/components/Restaurants/BellaItalia/BIGallery.jsx
+++ src/components/Restaurants/BellaItalia/BIGallery.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
-import Container from '../../common/Container'
-import ComponentTitle from '../../common/ComponentTitle'
-import PhotoGallery from '../../common/PhotoGallery'
+import React, { useState } from "react";
+import Container from "../../common/Container";
+import ComponentTitle from "../../common/ComponentTitle";
+import PhotoGallery from "../../common/PhotoGallery";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button,
+  useDisclosure,
+} from "@nextui-org/react";
 
 const imageGallery = [
   "/Bella-italia-img-1.png",
@@ -9,27 +18,84 @@ const imageGallery = [
   "/Bella-italia-img-3.png",
   "/Bella-italia-img-4.png",
   "/Bella-italia-img-5.png",
-  "/Bella-italia-img-1.png",
+  "/Bella-italia-img-1.2.png",
   "/Bella-italia-img-2.png",
-]
+];
 
 const BIGallery = () => {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const [currentImage, setCurrentImage] = useState(imageGallery[0]);
+
+  const handleThumbnailClick = (image) => {
+    setCurrentImage(image);
+  };
+
   return (
     <Container>
       <div>
-        <div>
-        <ComponentTitle children=" Discover our magnificent place in photos"
-        className="md:font-semibold"/>
-        <p className='max-w-[601px]'>The lorem ipsum is, in printing, a series of meaningless words used temporarily to calibrate a layout.The lorem ipsum is, in printing.</p>
+        <ComponentTitle
+          children="Discover our magnificent place in photos"
+          className="md:font-semibold"
+        />
+        <p className="max-w-[601px]">
+          The lorem ipsum is, in printing, a series of meaningless words used
+          temporarily to calibrate a layout. The lorem ipsum is, in printing.
+        </p>
 
-        <PhotoGallery 
-        mainPhoto="/Bella-italia-img-1.2.png"
-        photos={imageGallery}/>
-        </div>
+        {/* Thumbnail Gallery Component */}
+        <PhotoGallery photos={imageGallery} onPress={onOpen} />
 
+        {/* Modal */}
+        <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+          <ModalContent>
+            {(onClose) => (
+              <>
+                <ModalHeader className="flex flex-col gap-1">
+                  Gallery Preview
+                </ModalHeader>
+                <ModalBody>
+                  {/* Main Image */}
+                  <div className="relative mb-4">
+                    <img
+                      src={currentImage}
+                      alt="Main Photo"
+                      className="w-full h-auto object-cover rounded-lg"
+                    />
+                  </div>
+
+                  {/* Thumbnail Carousel */}
+                  <div className="overflow-x-auto flex gap-1">
+                    {imageGallery.map((image, index) => (
+                      <div
+                        key={index}
+                        className={`cursor-pointer border ${
+                          currentImage === image
+                            ? "border-blue-500"
+                            : "border-transparent"
+                        } rounded-lg`}
+                        onClick={() => handleThumbnailClick(image)}
+                      >
+                        <img
+                          src={image}
+                          alt={`Thumbnail ${index + 1}`}
+                          className="w-24 h-24 object-cover rounded-md"
+                        />
+                      </div>
+                    ))}
+                  </div>
+                </ModalBody>
+                <ModalFooter>
+                  <Button color="danger" variant="light" onPress={onClose}>
+                    Close
+                  </Button>
+                </ModalFooter>
+              </>
+            )}
+          </ModalContent>
+        </Modal>
       </div>
     </Container>
-  )
-}
+  );
+};
 
-export default BIGallery
+export default BIGallery;
--- src/components/common/PhotoGallery.jsx
+++ src/components/common/PhotoGallery.jsx
@@ -1,7 +1,7 @@
 import { Button } from "@nextui-org/react";
 import React from "react";
 
-const PhotoGallery = ({ photos }) => {
+const PhotoGallery = ({ photos, onPress }) => {
   return (
     <div className="relative py-10">
       <div className="grid grid-cols-4 grid-rows-2 sm:gap-4 sm3:gap-2 gap-1">
@@ -13,7 +13,7 @@ const PhotoGallery = ({ photos }) => {
             className="w-full h-full object-cover transition-transform duration-300 hover:scale-105"
           />
           {/* View All Photos Button */}
-          <Button className="absolute bottom-2 sm:bottom-4 sm:left-4 left-2 sm:px-4 px-2 h-7 sm:h-12 sm:text-sm text-[12px]  font-medium text-txtP bg-white rounded-full hover:bg-primary-dark transition">
+          <Button onPress={onPress} className="absolute bottom-2 sm:bottom-4 sm:left-4 left-2 sm:px-4 px-2 h-7 sm:h-12 sm:text-sm text-[12px]  font-medium text-txtP bg-white rounded-full hover:bg-primary-dark transition">
             View all photos ({photos.length})
           </Button>
         </div>
